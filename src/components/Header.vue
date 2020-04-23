cbfb448fbd42eecb5da5f79ad3079ab256ab9a64
Cristian <ecristian963@gmail.com>
2020-04-23 12:47:42 -0500

    Favicon y ajuste de barra superior

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,93 +1,95 @@
 <template>
   <div class="header-main">
     
-    <img width="140px" src="" />
+      <img width="60px" src="../assets/pagina3.png" />
 
-    <div class="header-buttons">
-      <!-- agregar imagenes de assets se hace con la forma de abajo -->
+ <!-- agregar imagenes de assets se hace con la forma de abajo -->
 
-    <button @click="ejemplo">esconder</button>
-    <button @click="reaparecer">aparecer</button>
-      <img src="../assets/categoria.png" alt="">
-      <input type="search" class="busqueda" placeholder="Buscar productos,marcas y mas...">
-      <img  v-if="muestraLogo" src="../assets/search.png" alt="search" />
-      <img src="../assets/account.png" alt="account" />
-      
+        <!-- <button @click="ejemplo">esconder</button>
+    <button @click="reaparecer">aparecer</button> -->
 
-      
-     
+      <div class="header-buttons">
+          <div class="menu">
+        <img src="../assets/categoria.png" alt="" />
+        <input
+          type="search"
+          class="busqueda"
+          placeholder="Buscar productos,marcas y mas..."/>
+        
+          <img v-if="muestraLogo" src="../assets/search.png" alt="search" />
+          <img src="../assets/account.png" alt="account" />
+        </div>
+      </div>
     </div>
-    
-  </div>
-  
   
 </template>
 
 <script>
 export default {
- 
   data() {
     return {
       muestraLogo: true,
-    }
+    };
   },
-   methods:{
-    reaparecer(){
+  methods: {
+    reaparecer() {
       this.muestraLogo = true;
     },
-    ejemplo(){
+    ejemplo() {
       this.muestraLogo = false;
-    }
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-
-.busqueda{
-
-display: flex;
-
-width: 526px;
-height: 32px;
-left: 457px;
-top: 14px;
-
-background: #F4ECEC;
-border-radius: 10px;
-
+.menu{
+  display: flex;
 }
-.iconsearch{
-display: flex;
-align-items: flex-end;
+.busqueda {
+  display: flex;
 
+  width: 526px;
+  height: 32px;
+  left: 457px;
+  top: 14px;
+
+  background: #f4ecec;
+  border-radius: 10px;
+}
+.iconsearch {
+  display: flex;
+  align-items: flex-end;
 }
 .header-main {
- /* Header */
+  /* Header */
 
-display: flex;
-align-items: center;
-height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+ 
 
-/* White Base */
+  height: 60px;
 
-background: #FFFFFF;
- position: absolute;
-left: -1px;
-right: -1px;
-top: -1px;
-bottom: -1px;
-border: 1px solid #4D94FF;
-display: flex;
+  /* White Base */
+
+  background: #ffffff;
+  position: absolute;
+  left: -1px;
+  right: -1px;
+  top: -1px;
+  bottom: -1px;
+  border: 1px solid #4d94ff;
+  display: flex;
 }
 .header-buttons {
   display: flex;
-  width: 1440px;
+
+  
   height: 45px;
   align-items: center;
   justify-content: space-between;
 }
-
 
 .button-support-border p {
   color: white;
